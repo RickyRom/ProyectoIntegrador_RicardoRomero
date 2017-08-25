3e197bbbb15e516fdaf7fa727036cec1cf9ba93e
RickyRom <30517351+RickyRom@users.noreply.github.com>
2017-08-24 19:56:19 -0600

    Avance con movimientos

--- ProyectoIntegrador_RicardoRomero/src/proyectointegrador_ricardoromero/Rey.java
+++ ProyectoIntegrador_RicardoRomero/src/proyectointegrador_ricardoromero/Rey.java
@@ -27,62 +27,103 @@ public class Rey extends Piezas {
     }
 
     public void setTipoR(String tipoR) {
-        this.tipoR = tipoR;
-    }
-
-    @Override
-    public String toString() {
-        return "Rey{" + "tipoR=" + tipoR + '}';
+        this.tipoR = "Rey";
     }
 
     @Override
     public String[][] movimiento(String[][] matriz, int x, int y, int x2, int y2) {
-        boolean direccion = true;
-        int centinela = 0;
-        if (x == x2 && y != y2) {
-            //Vertical
-            direccion = false;
-            for (int i = y; i < y2; i++) {
+        if ((x >= 0 && x <= 18 && y >= 0 && y <= 18) && (x2 >= 0 && x2 <= 18 && y2 >= 0 && y2 <= 18)) {
+            boolean direccion = true;
+            int centinela = 0;
+            if (x == x2 && y != y2) {
+                //H
+                direccion = false;
+                for (int i = y; i <= y2; i++) {
+                    if (matriz[x][i].contains("•") || matriz[x][i].contains("o")) {
+                        centinela = 1;
+                    } else {
 
-                if (!matriz[x][i].contains(" ") || matriz[x][i].contains("x")) {
-
-                } else {
-                    centinela = 1;
+                    }
                 }
-            }
-
-            if (centinela == 0) {
-                matriz[x2][y2] = matriz[x][y];
-                matriz[x][y] = " ";
-                return matriz;
-            } else {
-                System.out.println("Hay una pieza obstruyendo el movimiento!");
-                return matriz;
-            }
-        } else if (y == y2 && x != x2) {
-            //Horizontal
-            for (int i = x; i < x2; i++) {
-
-                if (!matriz[i][y].contains(" ") || matriz[x][i].contains("x")) {
-
+                if (centinela == 0) {
+                    if (x2 >= 2 && x2 <= 16 || y2 >= 2 && y2 <= 16) {
+                        matriz[x2][y2] = matriz[x][y];
+                        matriz[x][y] = " ";
+                        if (matriz[x2 + 1][y2].contains("•") && matriz[x2 + 2][y2].contains("o")) {
+                            matriz[x2 + 1][y2] = " ";
+                            System.out.println("Capturaste un Rebelde");
+                            return matriz;
+                        } else if (matriz[x2 - 1][y2].contains("•") && matriz[x2 - 2][y2].contains("o")) {
+                            matriz[x2 - 1][y2] = " ";
+                            System.out.println("Capturaste un Rebelde");
+                            return matriz;
+                        } else if (matriz[x2][y2 + 1].contains("•") && matriz[x2][y2 + 2].contains("o")) {
+                            matriz[x2][y2 + 1] = " ";
+                            System.out.println("Capturaste un Rebelde");
+                            return matriz;
+                        } else if (matriz[x2][y2 - 1].contains("•") && matriz[x2][y2 - 2].contains("o")) {
+                            matriz[x2][y2 - 1] = " ";
+                            return matriz;
+                        } else {
+                            return matriz;
+                        }
+                    } else {
+                        matriz[x2][y2] = matriz[x][y];
+                        matriz[x][y] = " ";
+                        return matriz;
+                    }
                 } else {
-                    centinela = 1;
+                    System.out.println("Obstrucción en el paso");
+                    return matriz;
                 }
-            }
+            } else if (y == y2 && x != x2) {
+                //V
+                for (int i = x; i <= x2; i++) {
+                    if (matriz[i][y].contains("•") || matriz[i][y].contains("o")) {
+                        centinela = 1;
+                    } else {
 
-            if (centinela == 0) {
-                matriz[x2][y2] = matriz[x][y];
-                matriz[x][y] = " ";
-                return matriz;
-            } else {
-                System.out.println("Hay una pieza obstruyendo el movimiento!");
+                    }
+                }
+
+                if (centinela == 0) {
+                    if (x2 >= 2 && x2 <= 16 || y2 >= 2 && y2 <= 16) {
+                        matriz[x2][y2] = matriz[x][y];
+                        matriz[x][y] = " ";
+                        if (matriz[x2 + 1][y2].contains("•") && matriz[x2 + 2][y2].contains("o")) {
+                            matriz[x2 + 1][y2] = " ";
+                            System.out.println("Capturaste un Rebelde");
+                            return matriz;
+                        } else if (matriz[x2 - 1][y2].contains("•") && matriz[x2 - 2][y2].contains("o")) {
+                            matriz[x2 - 1][y2] = " ";
+                            System.out.println("Capturaste un Rebelde");
+                            return matriz;
+                        } else if (matriz[x2][y2 + 1].contains("•") && matriz[x2][y2 + 2].contains("o")) {
+                            matriz[x2][y2 + 1] = " ";
+                            System.out.println("Capturaste un Rebelde");
+                            return matriz;
+                        } else if (matriz[x2][y2 - 1].contains("•") && matriz[x2][y2 - 2].contains("o")) {
+                            matriz[x2][y2 - 1] = " ";
+                            return matriz;
+                        } else {
+                            return matriz;
+                        }
+                    } else {
+                        matriz[x2][y2] = matriz[x][y];
+                        matriz[x][y] = " ";
+                        return matriz;
+                    }
+                }else{
+                    System.out.println("Obstrucción en el paso");
+                    return matriz;
+                }
+            }else{
+                System.out.println("No se puede mover en Diagonal");
                 return matriz;
             }
-        } else {
-            System.out.println("Intento moverse diagonalmente!");
+        }else{
+            System.out.println("Se Salió del Rango!");
             return matriz;
         }
-
     }
-
 }
